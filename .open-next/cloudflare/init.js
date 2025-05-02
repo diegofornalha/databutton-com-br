4f34ec5d2e12d2371d61356fd656cc079fc2c82f
Diego Fornalha <diegofornalha@gmail.com>
2025-05-02 11:59:43 -0300

    arquivos temporarios gitignore

--- .open-next/cloudflare/init.js
+++ .open-next/cloudflare/init.js
@@ -48,7 +48,7 @@ function initRuntime() {
   };
   Object.assign(globalThis, {
     Request: CustomRequest,
-    __BUILD_TIMESTAMP_MS__: 1745948431283,
+    __BUILD_TIMESTAMP_MS__: 1746197843171,
     __NEXT_BASE_PATH__: ""
   });
 }
